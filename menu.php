f7cfddd150f96458446fe72cf7ffcfef76a956b0
House boat <houseboatinkerala2014@gmail.com>
2015-07-11 12:54:48 +0530

    Adding menu

---
+++ menu.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+<meta charset="utf-8">
+	<meta property="fb:app_id" content="1429104807416551"/>
+	<meta property="og:title" content="Lagoonbliss.com"/>
+	<meta property="og:site_name" content="Lagoonbliss"/>
+	<meta property="og:description" content="Lagoonbliss offers backwater cruise by Houseboat in Alleppey and Kumarakom in Kerala, a safe and luxurious experience for couples, family and corporate groups."/>
+	<meta property="og:url" content="http://www.houseinkerala.in" />
+	<meta property="og:image" content="http:www.houseboatinkerala.in/resources/images/LagoonBliss.png" />
+<meta name="viewport" content="initial-scale=device-width">
+
+<META NAME="KEYWORDS" CONTENT="kumarakom, boat house kerala, kerala houseboat, houseboats india, kumarakom houseboat,alleppey houseboat, kerala houseboats, Boating kerala, houseboat cruise kerala, boathouse alleppey, Houseboats in kerala, house boat in kerala, houseboats in kerala,kerala backwater, backwaters of kerala alleppey, houseboats, houseboats holidays, houseboat vacation, luxury houseboats, HouseBoats, Houseboats in Kerala, Kerala Houseboat cruise, Kerala Houseboats night stay, Houseboat Cruises in Kerala, Allepey, kerala backwater houseboat, backwater houseboat kerala, kerala houseboat trip, house boat vacation in kerala, kerala luxury houseboats, houseboat accommodation, backwaters of kerala, kerala houseboat for family, kerala houseboat packages, kerala houseboat for honeymoon, kerala houseboat for rent, kerala houseboat tariff, kerala backwaters houseboat tour packages,hire houseboats,kerala backwaters tourism, houseboating kerala , kerala Houseboat premium catagory, Houseboat Kerala luxuries, Lagoonbliss, laggonbliss, lagoonblis, Lagoon bliss, Lagoonbliss Kerla Houseboat, lagoon bliss houseboat">
+
+<meta name="key-phrases" content="backwaters in kerala, houseboat booking, houseboat owner contact, best houseboat in alleppy, best houseboat in kerala, HouseBoats,Houseboats in Kerala, Kerala House Boats, Kerala Houseboats, boat house kerala, Kerala Houseboat Cruises, Houseboat Cruises in Kerala, Allepey,kerala house boats, kerala boat house, kerala houseboats, house boats in kerala, kerala backwater houseboat, backwater houseboat kerala, boat house in kerala, kerala houseboat, kerala house boat trip, house boat vacation in kerala, kerala boat house vacation, houseboats in kerala, kerala luxury houseboats, houseboat accommodation, kerala house boats tour, staying in kerala house boats, kerala boat house holiday, kerala backwater travel, backwaters travel in kerala, tourism in kerala, kerala vacation tour, kerala-tourism,Lagoonbliss, laggonbliss, lagoonblis, Lagoon bliss, Lagoonbliss Kerla Houseboat, lagoon bliss houseboat">
+
+	
+    <title>Lagoonbliss Kerala Houseboats, alleppey backwater cruise by houseboat in Kumarakom</title>
+	<link rel="icon" href="resources/images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="resources/css/basic.css"/>
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css"/>
+    <link rel="stylesheet" href="resources/css/custom_menu.css">
+	<link rel="stylesheet" href="resources/css/slider.css">
+    <!-- script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script-->
+	<!-- it works the same with all jquery version from 1.x to 2.x -->
+    <script type="text/javascript" src="resources/js/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="resources/js/main.js"></script>
+    <script type="text/javascript" src="resources/js/slider/core.js"></script>
+    <script type="text/javascript" src="resources/js/slider/utils.js"></script>
+    <script type="text/javascript" src="resources/js/slider/slider.js"></script>
+	
+	
+</head>
+<body>
+<div id="menu">
+        <div id='cssmenu'>
+            <div id="logo" title="domain name">
+				<!-- img src="resources/images/LagoonBliss.png" alt="LagoonBliss.com" style="height:50px" /-->
+			    <label style="color:green">Lagoon</label>
+				<label style="color:red">Bliss</label>
+				<label style="color:black" class="font-14" >.com</label>
+			</div>
+            <ul>
+               <li class='active menu-list'><span>Home</span></li>
+               <li class="menu-list"><label for="modal-1">About us<label></span></li>
+               <li class="menu-list">Gallery</span></li>
+			   <li class="menu-list"><a href="booktips.html">How to Book</a></li>
+               <li class="last menu-list"><label for="modal-2">Contact us</label></span></li>
+            </ul>
+        </div>
+    </div>
+</body>
